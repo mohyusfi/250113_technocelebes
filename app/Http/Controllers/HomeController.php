0ea710b0dd5474d7e0a236c5aaaa4c71ef8ee216
gitaadjah <gitaayu980@gmail.com>
2025-01-30 13:34:45 +0800

    dari gita

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,7 +11,7 @@ class HomeController extends Controller
     }
 
     public function service() {
-        return view('service');
+        return view('contents/services');
     }
 
     public function portofolio() {
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,53 +1,53 @@
-<nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container d-flex justify-content-beetwen">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+<body>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
 
-                <div class="collapse navbar-collapse ms-5" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto d-flex gap-5">
-                      <li>
-                        <a href="{{ route('home') }}">Home</a>
-                      </li>
-                      <li>
-                      <a href="{{ route('about') }}">About us</a>
-                      </li>
-                      <li>
-                      <a href="">Portofolio</a>
-                      </li>
-                      <li>
-                      <a href="">Services</a>
-                      </li>
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
+<nav class="navbar navbar-expand-lg shadow mb-5 bg-body">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="{{ route('home') }}">Cv. Techno Celebes</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <a class="nav-link active" href="{{ route('home') }}">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="{{ route('about') }}">About Us</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="">Portofolio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="{{ route('service') }}">Services</a>
+        </li>
+        @guest
+                        <li class="nav-item">
+                        <a class="nav-link active" href="{{ route('login') }}">Login</a>
+                        </li>
                             @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
+                            <li class="nav-item">
+                            <a class="nav-link active" href="{{ route('register') }}">Register</a>
+                            </li>
                             @endif
                         @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            <li class="nav-item">
+                                <a>
                                     {{ Auth::user()->name }}
                                 </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
+                                 <a class="nav-link active" href="{{ route('logout') }}"
+                                    onclick="event.preventDefault();
+                                    document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                         @csrf
@@ -55,7 +55,38 @@
                                 </div>
                             </li>
                         @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
+      </ul>
+    </div>
+</nav>
+<style>
+  a{
+    position: relative;
+    text-decoration: none;
+    color: black;
+    letter-spacing: 0.5px;
+    padding: 0 10px;
+  }
+  a:before,
+  a:after{
+    content: "";
+    position: absolute;
+    height: 3px;
+    width: 0;
+    background-color: blue;
+    transition: 0.5s;
+  }
+  a:after{
+    left: 0;
+    bottom: -4px;
+  }
+  a:before{
+    right: 0;
+    top: 0;
+  }
+  a:hover:after,
+  a:hover:before{
+    width: 100%;
+  }
+</style>
+</body>
+</html>
