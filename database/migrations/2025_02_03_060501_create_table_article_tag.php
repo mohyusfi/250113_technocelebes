1be894cfc0c637183f1b04cbb3c33bf3de75e213
Mohammad Yusfi Lakhafidun <mohammadyusfi039@gmail.com>
2025-02-03 14:38:10 +0800

    menambahkan route article dan portofolio

---
+++ database/migrations/2025_02_03_060501_create_table_article_tag.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableArticleTag extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('article_tag', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger("article_id")->nullable(false);
+            $table->unsignedBigInteger("tag_id")->nullable(false);
+            $table->foreign("article_id", "fk_article_id")->references("id_article")->on("articles");
+            $table->foreign("tag_id", "fk_tag_id")->references("id")->on("tags");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('article_tag');
+    }
+}
